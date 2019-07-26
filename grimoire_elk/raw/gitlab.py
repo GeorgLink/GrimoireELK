66c83ca1d018adc210d96908ec9e201aa22e34ce
Valerio Cosentino <valcos@bitergia.com>
2019-07-26 23:44:59 +0200

    [raw-gitlab] Change mapping
    This code changes the mapping of the raw items.
    The new mapping is the following:
    ```
        {
         "dynamic":true,
         "properties": {
            "data": {
               "dynamic":false,
               "properties": {}
           }
          }
        }
    ```
    
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- grimoire_elk/raw/gitlab.py
+++ grimoire_elk/raw/gitlab.py
@@ -32,37 +32,19 @@ class Mapping(BaseMapping):
         """Get Elasticsearch mapping.
 
         :param es_major: major version of Elasticsearch, as string
-        :returns:        dictionary with a key, 'items', with the mapping
+        :returns: dictionary with a key, 'items', with the mapping
         """
-
         mapping = '''
-         {
-            "dynamic":true,
+             {
+                "dynamic":true,
                 "properties": {
                     "data": {
-                        "properties": {
-                            "notes_data": {
-                                "dynamic":false,
-                                "properties": {
-                                    "body": {
-                                        "type": "text",
-                                        "index": true
-                                    }
-                                }
-                            },
-                            "description": {
-                                "type": "text",
-                                "index": true
-                            },
-                            "versions_data": {
-                                "dynamic":false,
-                                "properties": {}
-                            }
-                        }
+                        "dynamic":false,
+                        "properties": {}
                     }
                 }
-        }
-        '''
+            }
+            '''
 
         return {"items": mapping}
 
